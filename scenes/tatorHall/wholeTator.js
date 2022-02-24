daefd3a722bc1ccacb75c78bbc9e820e62167605
Harrison Dominique <54545693+hdominique11@users.noreply.github.com>
2022-02-24 16:21:58 -0500

    Cleaning Code
    Removed Classes that was not being used and cleaned up the "whole tator " class

--- scenes/tatorHall/wholeTator.js
+++ scenes/tatorHall/wholeTator.js
@@ -51,13 +51,13 @@ class wholeTator extends Phaser.Scene {
 
   }
   create() {
-    // setting the first room ID
-    //
-    this.roomTable = 1;
 
+    // setting the first room id
     this.roomIDDB = 1;
+    // settinh roomPicture variable
     this.roomPicture = "Not changed yet";
 
+    // setting currentRoomData to null
     this.currentRoomData = null;
 
 
@@ -67,58 +67,65 @@ class wholeTator extends Phaser.Scene {
     // button group
     this.backgroundGroup = this.add.group();
 
+    // adding the background
     this.background = this.add.image(400,300,"cafeFront0").setDepth(1);
     this.background.scale = .275;
     this.backgroundGroup.add(this.background);
+    //calling the setting button method
     this.setting();
+    // calling the navigation button method
     this.navigationButtons("wholeTator3","wholeTator","wholeTator1","wholeTator2");
-    //  this.setDatabaseConnection();
 
   }
 
           navigationButtons(sceneleft,sceneforward,sceneright,sceneback) {
 
-            // setting up a new background
+            // setting up the backwards button
             this.backNavigation = this.add.image(400,550, "backNavigation").setInteractive().setDepth(10);
             this.backNavigation.setScale(.3);
             this.backNavigation.on('pointerdown', () => {
               // changing the persons direction
               this.personDirection = (this.personDirection + 2) % 4;
-//              console.log(this.personDirection);
+
+//              calling the updateScene method that uses the Database
               this.updateSceneDB();
 
             });
-
+            // setting up the forward button
             this.forwardNavigation = this.add.image(400,450, "forwardNavigation").setInteractive().setDepth(10);
             this.forwardNavigation.setScale(.3);
             this.forwardNavigation.on('pointerdown', () => {
+              // calling the move forward scene that uses the database
               this.moveForwardSceneDB();
               //  this.scene.get(sceneforward).setLastLocation(this.scene.key);
               /*  this.scene.start(sceneforward),this*/
 
             });
-
+              // setting up the left button
             this.leftNavigation = this.add.image(350,500, "leftNavigation").setInteractive().setDepth(10);
             this.leftNavigation.setScale(.3);
             this.leftNavigation.on('pointerdown', () => {
-              //  this.scene.get(sceneleft).setLastLocation(this.scene.key);
-              // checks if the personDirection is 0. If its 0 set it to 3. If not subtract 1.
 
+              // checks if the personDirection is 0. If its 0 set it to 3. If not subtract 1.
+              // setting up the new players direction
               this.personDirection = (this.personDirection + 3) % 4;
-//              console.log(this.personDirection);
+//              calling the updateScene method that uses the Database
               this.updateSceneDB();
               /*  this.scene.start(sceneleft),this*/
             });
+              // setting up the right button
             this.rightNavigation = this.add.image(450,500, "rightNavigation").setInteractive().setDepth(10);
             this.rightNavigation.setScale(.3);
             this.rightNavigation.on('pointerdown', () => {
+                  // setting up the new players direction
               this.personDirection = (this.personDirection + 1) % 4;
-//              console.log(this.personDirection);
+//              calling the updateScene method that uses the Database
               this.updateSceneDB();
             });
 
           }
 
+          // method that sets the setting button
           setting() {
             this.settings_button = this.add.image(75,20, "settings_button").setInteractive().setDepth(10);
             this.settings_button.scale = .8;
@@ -134,9 +141,10 @@ class wholeTator extends Phaser.Scene {
                       // name of the new link needed
             const link = linkName[this.personDirection];
 
-            // getting the new room link
-//            console.log("Before it calls setRoomLink");
+            //gets the new roomID
             const roomID = this.setRoomLink(link);
+
+
 //            console.log("Finished the moveforward scene DB");
 
           }
@@ -145,11 +153,13 @@ class wholeTator extends Phaser.Scene {
             //gets the new room link
             const roomLink = await this.getRoomLink(link);
 
-//            console.log("This is the new room Link " + roomLink);
+//            if room link is not null and 0 then we set a new roomID
             if(roomLink != null && roomLink != 0) {
-//              console.log("Going to set new room link to " + roomLink);
+              // sets the new room id
               this.setRoomIDDB(roomLink);
+              // sets currentRoomData to null
               this.currentRoomData = null;
+              // calls update scene method
               this.updateSceneDB();
             }
 
@@ -158,60 +168,67 @@ class wholeTator extends Phaser.Scene {
 
 // gets the new room link
           async getRoomLink(link) {
+
+            // fetchs all the room data from fetchAllRoomData
             const data = await this.fetchAllRoomData();
-    //        console.log("This is the new link Direction " + link);
-  //          console.log(link);
-  //          console.log(data[link]);
-//            console.log("This is the variable for picture " + data["Link_East_ID"]);
+
+            // returns the new room id link
             return data[link];
 
           }
 
 
           async  updateSceneDB() {
-            //    this.backgroundGroup.clear(true);
+            // array of picture direction names
             const directionName = ["picNorth", "picEast", "picSouth", "picWest"];
+            // gets which direction name to get using the persons direction
             const direction = directionName[this.personDirection];
             //gets the room data
             await  this.setRoomPicture(direction);
-    //        console.log("End of the UpdateScene DB ");
+
           }
 
           // new function
           // gets the current Room data
           //
           async setRoomPicture(direction) {
+            // sets the picture name using the getRoomPicture method
             const pictureName = await this.getRoomPicture(direction);
+            // clears the background
             this.backgroundGroup.clear(true);
-    //        console.log("The Picture Name before putting it to the background " + pictureName);
+                  // setting up the new background using the information we got from the getRoomPicture method
             this.background = this.add.image(400,300,pictureName).setDepth(1);
             this.background.scale = .275;
             this.backgroundGroup.add(this.background);
-    //        console.log("code hit Adding Background");
+
 
 
           }
 
+          // gets the roomPicture Data
           async getRoomPicture(direction) {
+            //calls the fetchAllRoomData method to get all the room data
             const data = await this.fetchAllRoomData();
-      //      console.log("This is the Direction " + direction);
-      //      console.log(data);
-      //      console.log("This is the variable for picture " + data["picEast"]);
+// returns the new scene picture using the data and direction which is name for example picNorth
             return data[direction];
           }
 
 
           // grab the current room data
           async fetchAllRoomData() {
+            // if the currentroom data is not null then dont fetch data from database
             if (this.currentRoomData != null ) {
               return this.currentRoomData;
             }
+            // api url
             const urlRequest = ("http://localhost:3000/destination?Room_ID="+this.getRoomIDDB());
             try {
-        //      console.log("Fetch Room Data from "+ urlRequest);
+        //      fetch request using the api url
               const res = await fetch(urlRequest);
-      //     console.log("We Received a Response from the API");
+
+              // currentRoomData is now the fetch api response database
               this.currentRoomData = res.json();
+              // returns the currentRoomData
               return this.currentRoomData;
             }
             catch(err) {
@@ -227,10 +244,11 @@ class wholeTator extends Phaser.Scene {
             // this is the query request
             const urlRequest = ("http://localhost:3000/task?Task_ID="+this.getTaskID());
             try {
-        //      console.log("Fetch Room Data from "+ urlRequest);
+          //      fetch request using the api url
               const res = await fetch(urlRequest);
-      //     console.log("We Received a Response from the API");
+  // taskData is now the fetch api response database
               this.taskData = res.json();
+              //returns the taskdata
               return this.taskData;
             }
             catch(err) {
@@ -238,14 +256,6 @@ class wholeTator extends Phaser.Scene {
             }
           }
 
-          // sets the room ID
-          setRoomID(id) {
-            this.roomID = id;
-          }
-          // gets the room ID
-          getRoomID() {
-            return this.roomID;
-          }
 
           // sets the room ID
           setRoomIDDB(id) {
@@ -267,23 +277,29 @@ class wholeTator extends Phaser.Scene {
             //gets the task id
           }
 
+// sets the room picutre
           setRoomPicDB(roomPicture) {
             this.roomPicture = roomPicture;
           }
+          // gets the room picture
           getRoomPicDB() {
             return this.roomPicture;
           }
 
+          // gets the current room data
           getCurrentRoomData() {
             return this.currentRoomData;
           }
+          // gets the task data
           getTaskData() {
             return this.taskData;
           }
-
+ // gets the person direction
           getpersonDirection() {
             return this.personDirection;
           }
+          // sets the person direction
+          
           setpersonDirection(personDirection) {
             this.personDirection = personDirection;
           }
